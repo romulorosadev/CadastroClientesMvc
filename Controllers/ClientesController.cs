513e2ec5d2aa9c6640af13cb2c3a641ea7d1ee9e
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-07-02 22:15:05 -0300

    create controller cliente

--- Controllers/ClientesController.cs
+++ Controllers/ClientesController.cs
@@ -1,121 +1,32 @@
 using Microsoft.AspNetCore.Mvc;
-using CadastroClientesMvc.Data;
 using CadastroClientesMvc.Models;
 using System.Threading.Tasks;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 
-namespace CadastroDeClientes.Controllers
+namespace CadastroClientes.Controllers
 {
-  public class ClientesController : Controller
+  public class ClienteController : Controller
   {
-    private readonly ApplicationDbContext _context;
+    private readonly ClientesLista _clientesLista;
 
-    public ClientesController(ApplicationDbContext context)
+    public ClienteController(ClientesLista listaDeClientes)
     {
-      _context = context;
+      _clientesLista = ClientesLista;
     }
 
-    public async Task<IActionResult> Index()
-    {
-      return View(await _context.Clientes.ToListAsync());
-    }
-
+    [HttpGet]
     public IActionResult Create()
     {
       return View();
     }
 
     [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("ID_Cliente,Nome,Endereco,Telefone,RG,CPF,Email")] Cliente cliente)
+    public IActionResult Create(string nome, string endereco, string telefone, string rg, string cpf, string email)
     {
-      if (ModelState.IsValid)
-      {
-        _context.Add(cliente);
-        await _context.SaveChangesAsync();
-        return RedirectToAction(nameof(Index));
-      }
-      return View(cliente);
-    }
-
-    public async Task<IActionResult> Edit(int? id)
-    {
-      if (id == null)
-      {
-        return NotFound();
-      }
-
-      var cliente = await _context.Clientes.FindAsync(id);
-      if (cliente == null)
-      {
-        return NotFound();
-      }
-      return View(cliente);
-    }
-
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [Bind("ID_Cliente,Nome,Endereco,Telefone,RG,CPF,Email")] Cliente cliente)
-    {
-      if (id != cliente.ID_Cliente)
-      {
-        return NotFound();
-      }
-
-      if (ModelState.IsValid)
-      {
-        try
-        {
-          _context.Update(cliente);
-          await _context.SaveChangesAsync();
-        }
-        catch (DbUpdateConcurrencyException)
-        {
-          if (!ClienteExists(cliente.ID_Cliente))
-          {
-            return NotFound();
-          }
-          else
-          {
-            throw;
-          }
-        }
-        return RedirectToAction(nameof(Index));
-      }
-      return View(cliente);
-    }
-
-    public async Task<IActionResult> Delete(int? id)
-    {
-      if (id == null)
-      {
-        return NotFound();
-      }
-
-      var cliente = await _context.Clientes
-          .FirstOrDefaultAsync(m => m.ID_Cliente == id);
-      if (cliente == null)
-      {
-        return NotFound();
-      }
-
-      return View(cliente);
-    }
-
-    [HttpPost, ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-      var cliente = await _context.Clientes.FindAsync(id);
-      _context.Clientes.Remove(cliente);
-      await _context.SaveChangesAsync();
-      return RedirectToAction(nameof(Index));
-    }
-
-    private bool ClienteExists(int id)
-    {
-      return _context.Clientes.Any(e => e.ID_Cliente == id);
+      _clientesLista.AdicionarCliente(new Cliente { Nome = nome, Endereco = endereco, Telefone = telefone, RG = rg, CPF = cpf, Email = email });
+      return RedirectToAction("Index");
     }
   }
+
 }
