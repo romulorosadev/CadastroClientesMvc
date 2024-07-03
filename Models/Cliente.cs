13a3ae324a41ab73f0a88adbeafc9236a6a5eb3f
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-07-02 22:21:49 -0300

    create model cliente

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -11,3 +11,39 @@ public class Cliente
   public string Email { get; set; }
 
 }
+
+public class ClientesLista
+{
+  private List<Cliente> clientes = new List<Cliente>();
+
+  public void AdicionarCliente(Cliente cliente)
+  {
+    clientes.Add(cliente);
+  }
+
+  public List<Cliente> ObterClientes()
+  {
+    return clientes;
+  }
+
+  public Cliente ObterCliente(int id)
+  {
+    return clientes[id];
+  }
+
+  public void AtualizarCliente(int id, Cliente clienteAtualizado)
+  {
+    if (id >= 0 && id < clientes.Count)
+    {
+      clientes[id] = clienteAtualizado;
+    }
+  }
+
+  public void RemoverCliente(int id)
+  {
+    if (id >= 0 && id < clientes.Count)
+    {
+      clientes.RemoveAt(id);
+    }
+  }
+}
