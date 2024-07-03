c9d57bc75fb92041a1273b7e3cad9832cbbbd757
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-07-02 21:24:13 -0300

    configurar o serviço em programs.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,16 @@
+using CadastroClientesMvc.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 21))
+    )
+);
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -9,7 +19,6 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,6 +31,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Clientes}/{action=Index}/{id?}"
+);
 
 app.Run();
