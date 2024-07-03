338d987b2581c597329716041bea2a82252bc9b7
Romulo Guilherme Rosa | @romulorosadev <romulorosadev@gmail.com>
2024-07-02 21:57:57 -0300

    criacao model cliente

---
+++ Models/Cliente.cs
@@ -0,0 +1,13 @@
+
+namespace CadastroClientesMvc;
+public class Cliente
+{
+  public int ID_Cliente { get; set; }
+  public string Nome { get; set; }
+  public string Endereco { get; set; }
+  public string Telefone { get; set; }
+  public string RG { get; set; }
+  public string CPF { get; set; }
+  public string Email { get; set; }
+
+}
